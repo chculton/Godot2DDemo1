d4f19f628f812dba26fb4f52168a64e810961f29
chculton <chansculton@gmail.com>
2023-04-02 21:09:53 -0800

    Player updates;

---
+++ Player.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+public partial class Player : Area2D
+{
+	[Export]
+	public int speed = 400;
+
+	public Vector2 ScreenSize;
+	
+	public override void _Ready()
+	{
+		ScreenSize - GetViewportRect().Size;
+	}
+}
